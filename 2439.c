8fe6e723849e577380378a6091ddc07124b74111
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-09-28 15:02:44 +0900

    2439.c

--- 2439.c
+++ 2439.c
@@ -53,30 +53,57 @@
 
 
 //피라미드처럼 만들어보기 (정삼각형)
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int in;
+
+//     scanf("%d", &in);
+
+//     for(int i = 1; i <= in; i++)
+//     {
+//         for(int j = 1; j <= in - i; j++)
+//         {
+//             printf(" ");
+//         }
+//         for(int k = 1; k <= i; k++)
+//         {
+//             printf("*");
+//         }
+
+//         for(int q = 1; q < i; q++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+//
 #include <stdio.h>
 
 int main(void)
 {
-    int in;
+    int num;
 
-    scanf("%d", &in);
+    scanf("%d", &num);
 
-    for(int i = 1; i <= in; i++)
+    for(int i = 0; i < num; i++)
     {
-        for(int j = 1; j <= in - i; j++)
+        for(int j = 0; j < num - i -1;j++)
         {
             printf(" ");
         }
-        for(int k = 1; k <= i; k++)
-        {
-            printf("*");
-        }
-
-        for(int q = 1; q < i; q++)
+        for(int k = 0; k <= i; k++)
         {
             printf("*");
         }
         printf("\n");
+        
     }
+
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # C_lan_repeat
 repeat
+
+반복문
+2439(별찍기) 
+
+배열
+8958(OX퀴즈) : https://www.acmicpc.net/problem/8958
+
