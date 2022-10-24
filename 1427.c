44552f3cd9d9c5a98a343fb3c520b6f80a132d89
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-10-24 23:23:12 +0900

    1427.c

--- 1427.c
+++ 1427.c
@@ -1,31 +1,98 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(void)
+// {
+//     char num[11] = {0, };
+    
+//     scanf("%s", num);
+    
+//     int len;
+//     len = strlen(num);
+//     char temp;
+    
+//     for(int i = 0; i < len; i++)
+//     {
+//         for(int j = 0; j < len ; j++)
+//         {
+//             if((int)num[j] < (int)num[j+1])
+//             {
+//                 temp = num[j];
+//                 num[j] = num[j+1];
+//                 num[j+1] = temp;
+//             }
+//         }
+        
+//     }
+    
+//     printf("%s", num);
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+
+// int main(void)
+// {
+
+//     char N[10] = {0, };
+
+//     scanf("%s", N);
+
+//     int len;
+//     len = strlen(N);
+//     char temp;
+
+//     for(int i = 0; i < len; i++)
+//     {
+//         for(int j = 0; j < len; j++)
+//         {
+//             if((int)N[j] < (int)N[j+1]){
+//                 temp = N[j];
+//                 N[j] = N[j+1];
+//                 N[j+1] = temp;
+//             }
+//         }
+//     }
+
+//     printf("%s\n", N);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
-    char num[11] = {0, };
-    
-    scanf("%s", num);
-    
+
+    char n[11] = {0, };
+
+    scanf("%s", n);
+
     int len;
-    len = strlen(num);
+    len = strlen(n);
+
     char temp;
-    
+
     for(int i = 0; i < len; i++)
     {
-        for(int j = 0; j < len ; j++)
+        for(int j = 0; j < len; j++)
         {
-            if((int)num[j] < (int)num[j+1])
-            {
-                temp = num[j];
-                num[j] = num[j+1];
-                num[j+1] = temp;
+            if((int)n[j] < (int)n[j+1]){
+                temp = n[j];
+                n[j] = n[j+1];
+                n[j+1] = temp;
             }
         }
-        
     }
-    
-    printf("%s", num);
-    
+
+    printf("%s\n", n);
+
+
+
     return 0;
 }
